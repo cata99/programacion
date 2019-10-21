153d5d66aa82c3d8d86eddc93326c23c02950eab
cata99 <catupaschini@hotmail.com>
2019-10-21 19:26:44 -0300

    incio el repo con el main

---
+++ entrega/cmake-build-debug/CMakeFiles/entrega.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/entrega.dir/main.cpp.obj"
+  "entrega.pdb"
+  "entrega.exe"
+  "entrega.exe.manifest"
+  "libentrega.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/entrega.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ entrega/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+int main() {
+    ifstream in ;// comando para abrir el texto
+}
